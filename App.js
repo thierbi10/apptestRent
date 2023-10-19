96d674a7412ef8cb56a7952f6ebd87992d64a99e
thier <thierdiallodev@gmail.com>
2023-10-19 09:54:29 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>alhamdoulilahi !</Text>
+      <Image style={styles.containerIm} source={require('./assets/house4.jpg')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerIm: {
+    width:250,
+    height:250
+   
+  },
+});
